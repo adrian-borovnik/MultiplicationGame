bbca4fb3c72f590b49ab2cd8fc72bf490a2b881f
adrian456 <adrian.borovnik@student.um.si>
2023-10-21 21:54:40 +0200

    Add check input

--- app.js
+++ app.js
@@ -8,17 +8,31 @@ let secondFactor = 0
 let result = 0
 
 function init() {
-	generateEquation()
+	generateNewEquation()
 }
 
-function generateEquation() {
+function generateNewEquation() {
 	firstFactor = Math.ceil(Math.random() * 10)
 	secondFactor = Math.ceil(Math.random() * 10)
 
 	result = firstFactor * secondFactor
 
+	productInput.value = ''
+
 	firstFactorElement.innerHTML = firstFactor
 	secondFactorElement.innerHTML = secondFactor
 }
+
+productInput.addEventListener('keydown', (event) => {
+	let input = productInput.value
+
+	if (event.keyCode !== 13) return
+	if (!input) {
+		// TODO | indicate that the wrong answer was inputed
+		return
+	}
+
+	if (input == result) generateNewEquation()
+})
 
 init()
